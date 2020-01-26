789d8e4bb66b4f6a3d264a1f4bc89d982286e26b
Marcin Weglowski <marcin@escali.no>
2020-01-26 21:36:09 +0100

    Test user logging

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,7 +33,7 @@ class ModelTests(TestCase):
             )
 
     def test_create_new_super_user(self):
-        user = get_user_model().objects.create_super_user(
+        user = get_user_model().objects.create_superuser(
             email="test@email.com",
             password="P@$$w0rd"
         )
