d0e8acc7dc9b3c8a37e0bcbc339862013da390d1
Marcin Weglowski <marcin@escali.no>
2020-01-26 17:44:39 +0100

    Normalize emaail

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
         email = "test@email.com"
-        password = "P@ssw0rd"
+        password = "P@$$w0rd"
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -14,3 +14,13 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = "test@EMAIL.COM"
+        password = "P@$$w0rd"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
