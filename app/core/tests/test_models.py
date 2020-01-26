ee1efb099718eda76764d3d6444f56e21da97a9d
Marcin Weglowski <marcin@escali.no>
2020-01-26 20:49:43 +0100

    Verify if user email is provided.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,3 +24,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password="P@$$w0rd"
+            )
