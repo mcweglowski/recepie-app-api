eb973706106fdf92b10d517fa925acc8e91e2fc8
Marcin Weglowski <marcin@escali.no>
2020-02-06 11:48:00 +0100

    Password was too short

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -140,13 +140,13 @@ class PrivateUserApiTests(TestCase):
     def test_update_user_profile(self):
         payload = {
             "name": "new_name",
-            "password": "123",
+            "password": "new_password",
         }
 
         res = self.client.patch(ME_URL, payload)
 
         self.user.refresh_from_db()
 
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
