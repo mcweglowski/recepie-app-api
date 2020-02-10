1cafe4769fff28f9ef356a0b44aeef815310a2bb
Marcin Weglowski <marcin@escali.no>
2020-02-10 07:39:43 +0100

    Reciepe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -63,7 +63,7 @@ class ModelTests(TestCase):
         self.assertEqual(str(ingredient), ingredient.name)
 
     def test_recepie_str(self):
-        recepie = models.Recipe.objects.create(
+        recepie = models.Receipe.objects.create(
             user=sample_user(),
             title="Recepie Title",
             time_minutes=5,
