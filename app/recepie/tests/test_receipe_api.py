edf95bff207d5239cd904b1df52211c3c4111fcd
Marcin Weglowski <marcin@escali.no>
2020-02-12 07:24:46 +0100

    Recipe api tests

--- app/recepie/tests/test_receipe_api.py
+++ app/recepie/tests/test_receipe_api.py
@@ -5,12 +5,24 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recepie.serializers import RecipeSerializer
+from recepie.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 RECIPES_URL = reverse('recepie:recipe-list')
+
+
+def detail_url(recipe_id)
+    return reverse('recepie:recipe-detail', args=[recipe_id])
+
+
+def sample_tag(user, name="Main course"):
+    return Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name="Cinnamon"):
+    return Ingredient.objects.create(user=user, name=name)
 
 
 def sample_recepie(user, **params):
@@ -74,4 +86,16 @@ class PrivateRecepieApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        recipe = sample_recepie(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recepie.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+
         self.assertEqual(res.data, serializer.data)
